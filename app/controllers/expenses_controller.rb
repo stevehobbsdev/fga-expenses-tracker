e57246eed5114fa5f9d0536be8be16da49fc407a
Steve Hobbs <steve.hobbs@hey.com>
2024-06-05 13:06:57 +0100

    chore: use rspec-rails

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ExpensesController < ApplicationController
+  def index; end
+
+  def create; end
+
+  def update; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/create'
+  get 'expenses/update'
   get 'auth/login'
   get 'auth/logout'
   get 'auth/callback'
