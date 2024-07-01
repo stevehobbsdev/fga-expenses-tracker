2aabcf20936f8146fc323733c82fee73ec1f6382
Steve Hobbs <steve.hobbs@okta.com>
2024-07-01 11:22:47 +0100

    feat: complete team edit

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,7 +5,26 @@ class TeamsController < ApplicationController
     @teams = Team.order(:name)
   end
 
+  def new
+    @team = Team.new
+  end
+
   def edit
     @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    @team.update(team_params)
+
+    # Adjust FGA tuples depending on the value of :expense_approver
+
+    redirect_to action: :index if @team.valid?
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :expense_approver)
   end
 end
